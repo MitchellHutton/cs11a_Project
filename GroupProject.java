c22cc9d0dc8ee5de78b3c509bad103514ce4026c
szreich18 <szreich18@brandeis.edu>
2017-12-05 21:19:24 -0500

    added new CSV and changed file
    added new CSV and ch

--- GroupProject.java
+++ GroupProject.java
@@ -1,7 +1,7 @@
 public class GroupProject {
   public static void main(String[] args) {
 
-      int csvLength = 412;
+      int csvLength = 249;
 
       String[] player = new String[csvLength];    // col 0
       String[] pos = new String[csvLength];       // col 1
@@ -24,7 +24,7 @@ public class GroupProject {
       int teams = TextIO.getlnInt();
 
       // Read csv
-      TextIO.readFile("fantasy2.csv");
+      TextIO.readFile("fantasy.csv");
       TextIO.getln();  // skip the line of headers...
       int cate = 0; // categories
 
